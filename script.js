f259255c1df9c69127be60c94341322c10a17e9a
Ibrahim Ismail <ebrahemesmael@gmail.com>
2025-03-09 05:45:18 +0200

    init

---
+++ script.js
@@ -0,0 +1,182 @@
+let albums = [
+    {
+        id: 1,
+        title: "Inside the Blue",
+        artist: "RAGEH",
+        link: ""
+    },
+    {
+        id: 2,
+        title: "Shway Trooby",
+        artist: "ABOsahar",
+        link: ""
+    },
+    {
+        id: 3,
+        title: "Pink Tape",
+        artist: "Bashar Suliman X Drummer B",
+        link: ""
+    },
+    {
+        id: 4,
+        title: "captain solo",
+        artist: "youssef abouzeid",
+        link: ""
+    },
+    {
+        id: 5,
+        title: "Throwbacks",
+        artist: "Bashar Suleiman",
+        link: ""
+    },
+    {
+        id: 6,
+        title: "KOMBILE 001",
+        artist: "HIZZ",
+        link: ""
+    },
+    {
+        id: 7,
+        title: "LSTEFA",
+        artist: "KZLK",
+        link: ""
+    },
+    {
+        id: 8,
+        title: "SADSUN",
+        artist: "INSIN",
+        link: ""
+    },
+    {
+        id: 9,
+        title: "∞σ",
+        artist: "DG Hadi",
+        link: ""
+    },
+    {
+        id: 10,
+        title: "The Caveman",
+        artist: "ABOsahar",
+        link: ""
+    },
+    {
+        id: 11,
+        title: "pink stm & wite ptl",
+        artist: "J!N",
+        link: ""
+    },
+    {
+        id: 12,
+        title: "KOMBILE 002",
+        artist: "HIZZ",
+        link: ""
+    },
+    {
+        id: 13,
+        title: "fuzzylittlebrain",
+        artist: "fuzzylittlebrain",
+        link: ""
+    },
+    {
+        id: 14,
+        title: "aho",
+        artist: "youssef abouzeid",
+        link: ""
+    },
+    {
+        id: 15,
+        title: "Foreigner",
+        artist: "JIN",
+        link: ""
+    },
+    {
+        id: 16,
+        title: "Attraction",
+        artist: "DAHB",
+        link: ""
+    },
+    {
+        id: 17,
+        title: "Anterior",
+        artist: "Timmy Mowafi",
+        link: ""
+    },
+    {
+        id: 18,
+        title: "BAD SECTOR",
+        artist: "[machine.condition]",
+        link: ""
+    },
+    {
+        id: 19,
+        title: "Entity",
+        artist: "Gast",
+        link: ""
+    },
+    {
+        id: 20,
+        title: "Shuttle Juice",
+        artist: "Hebatallah",
+        link: ""
+    },
+    {
+        id: 21,
+        title: "Ma2ali",
+        artist: "ARRAKK",
+        link: ""
+    },
+    {
+        id: 22,
+        title: "/many sins, mini synths.",
+        artist: "Centum/ˈsɛntəm/",
+        link: ""
+    },
+    {
+        id: 23,
+        title: "Mechanical Love",
+        artist: "J!N",
+        link: ""
+    },
+    {
+        id: 24,
+        title: "Baytee Baytak",
+        artist: "Electric*té Du Liban",
+        link: ""
+    },
+    {
+        id: 25,
+        title: "MoultingKICK_23652",
+        artist: "Centum/ˈsɛntəm/",
+        link: ""
+    },
+    {
+        id: 26,
+        title: "KOMBILE 003",
+        artist: "HIZZ",
+        link: ""
+    },
+    {
+        id: 27,
+        title: "تلاته",
+        artist: "Lotfy",
+        link: ""
+    },
+    {
+        id: 28,
+        title: "DG Hadi",
+        artist: "bb",
+        link: ""
+    },
+    {
+        id: 29,
+        title: "Baytee Baytak",
+        artist: "TassTooz",
+        link: ""
+    },
+    {
+        id: 30,
+        title: "باندا",
+        artist: "سته ستميه باند",
+        link: ""
+    }
+];
