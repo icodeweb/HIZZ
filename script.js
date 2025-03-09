20eb1380338cb5c7857ffa763fd9dd1314603bd4
Ibrahim Ismail <ebrahemesmael@gmail.com>
2025-03-09 08:57:48 +0200

    basic styling and display the albums and added covers

--- script.js
+++ script.js
@@ -3,180 +3,438 @@ let albums = [
         id: 1,
         title: "Inside the Blue",
         artist: "RAGEH",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/inside-the-blue"
     },
     {
         id: 2,
         title: "Shway Trooby",
         artist: "ABOsahar",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/shway-trooby"
     },
     {
         id: 3,
         title: "Pink Tape",
         artist: "Bashar Suliman X Drummer B",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/pink-tape"
     },
     {
         id: 4,
         title: "captain solo",
         artist: "youssef abouzeid",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/captain-solo"
     },
     {
         id: 5,
         title: "Throwbacks",
         artist: "Bashar Suleiman",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/throwbacks"
     },
     {
         id: 6,
         title: "KOMBILE 001",
         artist: "HIZZ",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/kombile-001"
     },
     {
         id: 7,
         title: "LSTEFA",
         artist: "KZLK",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/lstefa"
     },
     {
         id: 8,
         title: "SADSUN",
         artist: "INSIN",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/sadsun"
     },
     {
         id: 9,
         title: "∞σ",
         artist: "DG Hadi",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/-"
     },
     {
         id: 10,
         title: "The Caveman",
         artist: "ABOsahar",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/the-caveman"
     },
     {
         id: 11,
         title: "pink stm & wite ptl",
         artist: "J!N",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/pink-stm-wite-ptl-2"
     },
     {
         id: 12,
         title: "KOMBILE 002",
         artist: "HIZZ",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/kombile-002"
     },
     {
         id: 13,
         title: "fuzzylittlebrain",
         artist: "fuzzylittlebrain",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/fuzzylittlebrain"
     },
     {
         id: 14,
         title: "aho",
         artist: "youssef abouzeid",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/aho"
     },
     {
         id: 15,
         title: "Foreigner",
         artist: "JIN",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/foreigner"
     },
     {
         id: 16,
         title: "Attraction",
         artist: "DAHB",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/attraction"
     },
     {
         id: 17,
         title: "Anterior",
         artist: "Timmy Mowafi",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/anterior"
     },
     {
         id: 18,
         title: "BAD SECTOR",
         artist: "[machine.condition]",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/bad-sector"
     },
     {
         id: 19,
         title: "Entity",
         artist: "Gast",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/entity"
     },
     {
         id: 20,
         title: "Shuttle Juice",
         artist: "Hebatallah",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/shuttle-juice"
     },
     {
         id: 21,
         title: "Ma2ali",
         artist: "ARRAKK",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/ma2ali"
     },
     {
         id: 22,
-        title: "/many sins, mini synths.",
+        title: "many sins, mini synths",
         artist: "Centum/ˈsɛntəm/",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/many-sins-mini-synths"
     },
     {
         id: 23,
         title: "Mechanical Love",
         artist: "J!N",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/mechanical-love"
     },
     {
         id: 24,
-        title: "Baytee Baytak",
-        artist: "Electric*té Du Liban",
-        link: ""
+        title: "Electric*té Du Liban",
+        artist: "Baytee Baytak",
+        link: "https://hizzme.bandcamp.com/album/electric-t-du-liban"
     },
     {
         id: 25,
         title: "MoultingKICK_23652",
         artist: "Centum/ˈsɛntəm/",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/moultingkick-23652"
     },
     {
         id: 26,
         title: "KOMBILE 003",
         artist: "HIZZ",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/kombile-003"
     },
     {
         id: 27,
         title: "تلاته",
         artist: "Lotfy",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/--2"
     },
     {
         id: 28,
-        title: "DG Hadi",
-        artist: "bb",
-        link: ""
+        title: "bb",
+        artist: "DG Hadi",
+        link: "https://hizzme.bandcamp.com/album/bb"
     },
     {
         id: 29,
-        title: "Baytee Baytak",
-        artist: "TassTooz",
-        link: ""
+        title: "TassTooz",
+        artist: "Baytee Baytak",
+        link: "https://hizzme.bandcamp.com/album/tasstooz"
     },
     {
         id: 30,
         title: "باندا",
         artist: "سته ستميه باند",
-        link: ""
+        link: "https://hizzme.bandcamp.com/album/--4"
     }
 ];
+let events = [
+    {
+        title: "Khaleet 001 / خليط ٠٠١",
+        date: "Wednesday, December 13, 2017",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "Khaleet 002 / خليط ٠٠٢",
+        date: "Friday, December 29, 2017",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "Khaleet 003 / خليط ٠٠٣",
+        date: "Saturday, January 13, 2018",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "Thursday Night Beat Down feat. R A G E H",
+        date: "Thursday, January 18, 2018",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "Khaleet 004 / خليط ٠٠٤",
+        date: "Friday, January 26, 2018",
+        location: "Uncle Sam Pub, Amman"
+    },
+    {
+        title: "Drummer B x ONSY x Bashar Suleiman",
+        date: "Wednesday, March 14, 2018",
+        location: "Zigzag, Cairo"
+    },
+    {
+        title: "Khaleet 005 / خليط ٠٠٥",
+        date: "Wednesday, April 25, 2018",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "Khaleet 006 / خليط ٠٠٦",
+        date: "Sunday, May 27, 2018",
+        location: "Kiesling, Detroit"
+    },
+    {
+        title: "Livestream w Youssef Abouzeid X Bashar Suleiman X Torkhy",
+        date: "Friday, July 20, 2018 @ 7:00PM",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "INSIN - Livestream",
+        date: "Wednesday, August 8, 2018",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "Youssef Abouzeid 'Captain Solo' Release Party",
+        date: "Wednesday, August 29, 2018 @ 7:00PM",
+        location: "Bardo Clubhouse, Maadi"
+    },
+    {
+        title: "assembli X Hizz present Cairo Invasion",
+        date: "Friday, November 16, 2018",
+        location: "MENA Tyche Hotel, Amman"
+    },
+    {
+        title: "IV IV أربعة أربعة",
+        date: "Sunday, February 3, 2019",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "Weight of the Heart :: Bana Kabalan",
+        date: "Thursday, March 21, 2019",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "Shake and Destroy :: DJ Haram x Drummer B x Bashar Suleiman",
+        date: "Thursday, March 28, 2019",
+        location: "Unicorn Garden, Cairo"
+    },
+    {
+        title: "Record Store Day OPEN HOUSE",
+        date: "Saturday, April 13, 2019",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "Open House",
+        date: "Saturday, July 27, 2019",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "Kafr El-Dauwar Records X HIZZ (Open House)",
+        date: "Saturday, August 10, 2019",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "J!N Album Release",
+        date: "Saturday, September 14, 2019",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "Ya Khal Album Release",
+        date: "Thursday, October 17, 2019",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "Fuzzylittlebrain Album Release",
+        date: "Friday, November 15, 2019",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "HIZZ Detroit Black Friday Grand Opening",
+        date: "Friday, November 29, 2019",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "JessicaSligter + OwenGardner LIVE",
+        date: "Friday, November 29, 2019",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "TAKH Album Release",
+        date: "Thursday, December 5, 2019",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "Slovvdk سلوديكاي",
+        date: "Friday, December 13, 2019",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "MauriceLouca + MaxtonFort",
+        date: "Friday, January 10, 2020",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "amélie legrand ft Alan Bishop + mmmalak",
+        date: "Friday, January 10, 2020",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "FredericOberland + GregoryDargent + guests",
+        date: "Thursday, January 16, 2020",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "TURQUOISE DESERT RESEARCH PROGRAMME PRESENTATIONS + AFTERPARTY",
+        date: "Monday, January 20, 2020",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "Juliana Huxtable X Nadah El Shazly X KZLK",
+        date: "Tuesday, January 21, 2020",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "Kid Fourteen + Prof.k",
+        date: "Friday, January 24, 2020",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "الشُك",
+        date: "Saturday, February 15, 2020",
+        location: "HIZZ, Cairo"
+    },
+    {
+        title: "25 Maadi",
+        date: "Friday, February 25, 2022",
+        location: "Maadi, Cairo"
+    },
+    {
+        title: "Abosahar at CJC Cairo",
+        date: "Saturday, July 20, 2024",
+        location: "Cairo Jazz Club (CJC), Cairo, Egypt"
+    },
+    {
+        title: "HIZZ Tour: Yverdon",
+        date: "Saturday, August 10, 2024",
+        location: "Festival Le Castrum, Yverdon, Switzerland"
+    },
+    {
+        title: "HIZZ Tour: Montpellier",
+        date: "Monday, August 12, 2024",
+        location: "Montpellier, France"
+    },
+    {
+        title: "HIZZ Tour: Toulouse",
+        date: "Tuesday, August 13, 2024",
+        location: "Troisième Loutre, Toulouse"
+    },
+    {
+        title: "HIZZ Tour: Pau",
+        date: "Wednesday, August 14, 2024",
+        location: "Galaxie 23 x Live, Pau, France"
+    },
+    {
+        title: "HIZZ Tour: Geneva",
+        date: "Friday, August 16, 2024",
+        location: "Théâtre de l’Orangerie, Geneva, Switzerland"
+    },
+    {
+        title: "Abosahar at Chilloposite Festival",
+        date: "Saturday, October 5, 2024",
+        location: "Dahab, EG"
+    },
+    {
+        title: "HIZZ Tour: Rotterdam",
+        date: "Friday, October 11, 2024",
+        location: "Radicalisms Festival, Rotterdam, Netherlands"
+    },
+    {
+        title: "HIZZ",
+        date: "Thursday, October 17, 2024",
+        location: "La Chaux d’Ouvron, Geneva, Switzerland"
+    },
+    {
+        title: "HIZZ Tour: Lyon",
+        date: "Friday, October 18, 2024",
+        location: "Sonic 10, Lyon, France"
+    },
+    {
+        title: "HIZZ Tour: Amsterdam",
+        date: "Saturday, October 19, 2024",
+        location: "Garage Noord, Amsterdam, Netherlands"
+    },
+    {
+        title: "Abosahar at CJC",
+        date: "Saturday, October 26, 2024",
+        location: "Cairo Jazz Club (CJC), Cairo, Egypt"
+    },
+    {
+        title: "Abosahar at CJC Cairo",
+        date: "Saturday, December 21, 2024",
+        location: "Cairo Jazz Club (CJC), Cairo, Egypt"
+    }
+];
+
+
+let albumsContiner = document.querySelector(".albums");
+
+let albumsHTML = ``;
+albums.forEach(album => {
+    albumsHTML += `
+                <a class="album" id="album${album.id}" href="${album.link}">
+                    <img src="/assets/imgs/albumcovers/${album.title}.webp" alt="${album.title}">
+                    <div class="albumInfo">
+                    <span class="albumTitle">${album.title}</span>
+                    <span class="albumArtist">${album.artist}</span>
+                    </div>
+                    </a>`
+})
+
+albumsContiner.innerHTML = albumsHTML
+
+
+
+// let linksArray = [];
+// let links = document.querySelectorAll("a");
+// links.forEach(a => {
+//     linksArray.push(a.href)
+// })
+// console.log(linksArray);
